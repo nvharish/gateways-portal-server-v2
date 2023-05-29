f6582b2ab02cb8aba3d884ef8eea9506847778f7
N V Harish <harishnv1991@gmail.com>
2023-05-29 23:48:27 +0530

    node project structure created

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var http = require("http");
+http.createServer(function (req, res) {
+    if(req.url==='/')
+        res.end("<h1>Hello world</h1>");
+}).listen(8900, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server is running");
+    }
+});
