6ac541d425fe84795e19a3215008e62b611d557d
N V Harish <harishnv1991@gmail.com>
2023-06-02 01:03:57 +0530

    test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var http = require("http");
 http.createServer(function (req, res) {
     if(req.url==='/')
-        res.end("<h1>Hello world</h1>");
+        res.end("<h1>Hello world from dev</h1>");
 }).listen(8900, function (err) {
     if (err) {
         console.log(err);
